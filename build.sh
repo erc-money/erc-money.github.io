58d5e259c9ea24240838b850a04f0a7af9d9e2df
AlexanderC <alexander.moldova@gmail.com>
2020-11-30 17:42:53 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ if [ -z "$COMMIT_MSG" ]; then
   COMMIT_MSG="New build"
 fi
 
-# echo "Build Dapp"
-# cd "$SCRIPT_DIR/vapp" && npm run build
+echo "Build Dapp"
+cd "$SCRIPT_DIR/vapp" && npm run build
 
-# cd "$SCRIPT_DIR/docs"
-# mkdir docs
-# cd ./docs
+cd "$SCRIPT_DIR/docs"
+mkdir docs
+cd ./docs
 
-# echo "Include Solidity Metrics"
-# cp ../../solidity-metrics.html ./
+echo "Include Solidity Metrics"
+cp ../../solidity-metrics.html ./
 
-# echo "Include Coverage Report"
-# cp -R ../../coverage ./
+echo "Include Coverage Report"
+cp -R ../../coverage ./
 
 cd "$SCRIPT_DIR"
 read -r -p "Would you like to push changes to Github >> '$COMMIT_MSG'? [Y/n] " _response
